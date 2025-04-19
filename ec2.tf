8e3137ac780511a0f20b1f8d77945bccef312343
Kotesh Vepuri <j_vepuri@yahoo.com>
2025-04-19 17:51:33 -0400

    terraform ec2 update

--- ec2.tf
+++ ec2.tf
@@ -1,17 +1,17 @@
 # configured aws provider with proper credentials
 provider "aws" {
   region  = "us-east-1"
-  profile = "terraform-user"
+  profile = "terrauser"
 }
 
 
 # store the terraform state file in s3
 terraform {
   backend "s3" {
-    bucket  = "aosnote-terraform-state-bucket"
+    bucket  = "terraform-state-vepuri-remote"
     key     = "build/terraform.tfstate"
     region  = "us-east-1"
-    profile = "terraform-user"
+    profile = "terrauser"
   }
 }
 
@@ -97,7 +97,7 @@ resource "aws_instance" "ec2_instance" {
   instance_type          = "t2.micro"
   subnet_id              = aws_default_subnet.default_az1.id
   vpc_security_group_ids = [aws_security_group.ec2_security_group.id]
-  key_name               = "myec2key"
+  key_name               = "terrakey"
   user_data              = file("install_techmax.sh")
 
   tags = {
